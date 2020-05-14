4e6264ffeac9f5e5bc986ae2446ae95aad157aaf
andre lawrence <andrelawrence35@gmail.com>
2020-05-14 17:01:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,8 +14,8 @@ end
 
 def assign_rooms(attendees)
 attendees.each_with_index do |name, index|
-  return ["Hello, #{name}! You'll be assigned to room #{index+1}!"]
-
+  puts ["Hello, #{name}! You'll be assigned to room #{index+1}!"]
+  
 end
 
 def printer(attendees)
