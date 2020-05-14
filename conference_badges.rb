fd32c286f9573e3b0d14d6b74985fc124328fc88
andre lawrence <andrelawrence35@gmail.com>
2020-05-14 17:01:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,7 @@ attendees.each_with_index do |name, index|
   puts ["Hello, #{name}! You'll be assigned to room #{index+1}!"]
   
 end
+end
 
 def printer(attendees)
    batch_badge_creator(attendees).each { |names| puts names }
