7622197d766ecbc7177e5502fdaf2e0020becb23
andre lawrence <andrelawrence35@gmail.com>
2020-05-14 16:43:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,15 @@ end
 
 def assign_rooms(attendees)
 attendees.each_with_index do |name, index|
-  
+  array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+   end
+   array
+end
+
+def printer(attendees)
+   batch_badge_creator(attendees).each { |names| puts names }
+   assign_rooms(attendees).each { |room| puts room }
+end  
 
   
 end 
